cf535a41794212cad8a9d0707f52fc0e08483726
priya05122000 <npriyanagendran2000@gmail.com>
2026-06-25 17:17:33 +0530

    fixes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -52,7 +52,7 @@ export default async function HomePage() {
 
             {/* <HotelCTASection /> */}
 
-            <CinematicSplitSection />
+            {/* <CinematicSplitSection /> */}
 
             <Testimonials reviews={guestReviewData.data} />
             <FAQSection faqDatas={faqDatas.data} faqCategories={faqCategories.data} />
