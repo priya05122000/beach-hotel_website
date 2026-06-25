1f5f2d502726f0bca0c4d651946a717598376afd
priya05122000 <npriyanagendran2000@gmail.com>
2026-06-25 13:56:43 +0530

    fixes

--- src/app/components/LenisProvider.tsx
+++ src/app/components/LenisProvider.tsx
@@ -13,7 +13,7 @@ export default function LenisProvider({ children }: { children: React.ReactNode 
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: true,
+      syncTouch: true,
       touchMultiplier: 2,
     });
 
