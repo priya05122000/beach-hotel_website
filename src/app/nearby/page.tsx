91b087c6fb0d47fc77de7906c9ed7e5fefeafc6e
04-AKILESH <akileshnagarajan123@gmail.com>
2026-06-26 12:42:48 +0530

    Nearby

---
+++ src/app/nearby/page.tsx
@@ -0,0 +1,14 @@
+import { getNearbyDestinationData } from "@/src/service/nearbyDestination";
+import NearbyHeroBanner from "./components/NearbyHeroBanner";
+import NearbyDestinationsSection from "./components/NearbyDestinationsSection";
+
+export default async function NearbyPage() {
+  const { data: destinations } = await getNearbyDestinationData();
+
+  return (
+    <div>
+      <NearbyHeroBanner destinations={destinations} />
+      <NearbyDestinationsSection destinations={destinations} />
+    </div>
+  );
+}
