8e448aa7dd442b6988dd658d70ad4c39082a2d9d
priya05122000 <npriyanagendran2000@gmail.com>
2026-06-30 05:15:59 +0530

    fixes

--- src/app/(site)/blog/[slug]/components/BlogIntro.tsx
+++ src/app/(site)/blog/[slug]/components/BlogIntro.tsx
@@ -20,8 +20,8 @@ export default function BlogIntro({ blog }: Props) {
   return (
     <Section className="pt-16 lg:pt-20">
       <div className="border-b border-silver pb-10">
-        <div className="pt-1 pr-20 float-left">
-          <p className="text-gray type-overline font-arizona-sans-regular tracking-[0.25em] ">
+        <div className="pt-1 pr-20 sm:float-left">
+          <p className="text-gray type-overline font-arizona-sans-regular tracking-[0.25em] mb-3 sm:mb-0">
             {blog.tag_1 ?? "Article"}
           </p>
         </div>
@@ -30,7 +30,7 @@ export default function BlogIntro({ blog }: Props) {
           <div className="overflow-hidden">
             <h3
               ref={titleRef}
-              className="type-h2  text-primary-dark leading-snug"
+              className="type-h2 text-primary-dark leading-snug"
             >
               {blog.sub_title}
             </h3>
@@ -38,8 +38,8 @@ export default function BlogIntro({ blog }: Props) {
         </div>
       </div>
 
-      <div className="grid md:grid-cols-[0.8fr_1fr] pt-10 gap-3">
-        <p className="text-gray tracking-[0.25em] type-body uppercase ">{blog.tag_2}</p>
+      <div className="grid grid-cols-1 md:grid-cols-[0.8fr_1fr] pt-10 gap-y-6 gap-x-3">
+        <p className="text-gray tracking-[0.25em] type-body uppercase">{blog.tag_2}</p>
         <div className="flex flex-col gap-6 md:gap-10">
           <div className="blog-content text-charcoal leading-snug max-w-md" dangerouslySetInnerHTML={{ __html: blog.description_1 }} />
           <div className="border-t border-silver" />
