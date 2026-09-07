ab574dc72d09586aa89bc8315e54383abf9fafad
04-AKILESH <akileshnagarajan123@gmail.com>
2026-09-07 14:14:56 +0530

    Loader

--- src/components/common/PageLoader.tsx
+++ src/components/common/PageLoader.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
+
 export default function PageLoader() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-cream">
             <div className="flex flex-col items-center gap-4">
-                <div className="w-10 h-10 border-2 border-primary/20 border-t-primary rounded-full animate-spin" />
-                <p className="text-primary-dark/50 text-xs tracking-[0.2em] uppercase font-light">
-                    Loading
-                </p>
+                <Image
+                    src="/loader/icn_1.gif"
+                    alt="Loading"
+                    width={80}
+                    height={80}
+                    unoptimized
+                    priority
+                    className="w-20 h-20 object-contain"
+                />
             </div>
         </div>
     );
